66543bd3fc9dc8d64b4bd30dc0746c15063091e7
Yami Pedroso <yamistudio@mac.home>
2025-02-07 20:25:56 +0100

    New collection dev

--- src/components/book-slider/book-slider-collection/BookSliderCollection.tsx
+++ src/components/book-slider/book-slider-collection/BookSliderCollection.tsx
@@ -3,6 +3,7 @@ import { bookSliderData } from '../../../data/bookSlider'
 import UserProfilAccount from '../../common/user-profil-account/UserProfilAccount'
 import Marquee from 'react-marquee-slider'
 import './styles.css'
+import { motion } from 'framer-motion'
 
 interface IBookSliderData {
   id: number
@@ -14,12 +15,21 @@ interface IBookSliderData {
 }
 
 const BookSliderCollection = () => {
+  const [hover, setHover] = useState(false)
   const [bookSliderCollData, setBookSliderCollData] = useState<
     IBookSliderData[]
   >(bookSliderData)
   const [textActive, setTextActive] = useState(false)
   const [hoveredBookIndex, setHoveredBookIndex] = useState<number | null>(null)
   const [stopFirstBookMovement, setStopFirstBookMovement] = useState(false)
+
+  const handleNewColHover = () => {
+    setHover(true)
+  }
+
+  const handleNewColLeave = () => {
+      setHover(false)
+  }
 
   const handleMouseOver = (index: number) => {
     setHoveredBookIndex(index)
@@ -140,10 +150,48 @@ const BookSliderCollection = () => {
       <div className="counter-books-new-coll-wrapper">
         <div className="new-coll-wrapper">
           <p>
-            Got chance to read out the <a href="#"> new collection </a> of Harry
+            Got chance to read out the <a href="#"
+              onMouseEnter={handleNewColHover}
+              onMouseLeave={handleNewColLeave}
+            > new collection </a> of Harry
             Potter? It's a must-read for any fan of the series, don't miss it!
             :)
           </p>
+
+          <motion.div
+             initial={{ opacity: 0, x: 50 }}
+             animate={{ opacity: hover ? 1 : 0, x: hover ? 0 : -50 }}
+             exit={ { opacity: 0, x: -50 } }
+             transition={{ duration: 0.5, ease: "easeInOut" }}
+             style={{ 
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "200px",
+              color: "#1a1a1a", 
+              padding: "1rem", 
+              background: "#fff", 
+              boxShadow: "3px 3px 30px 1px rgba(56, 87, 138, 0.3)", 
+              visibility: hover ? "visible" : "hidden",
+              position: "absolute",
+              right: "-2.5rem",
+              top: "4.5rem"
+            }}>
+
+            <img src="https://media.takealot.com/covers_tsins/35774288/35774288-1VOC-zoom.jpg" alt="harry-potter"
+              style={{ width: "80px", height: "80px"}}
+             />
+            <p
+              style={{ 
+                marginLeft: ".3rem",
+                fontSize: "0.8rem",
+                textAlign: "center",
+                fontWeight: "bold",
+                color: "#f27373"
+      
+              }}
+            >Harry Potter collection</p>
+          </motion.div>
         </div>
         <div className="books-counter">
           <p>{bookSliderCollData.length} books</p>
